7f8675d82bc6b979315f569af79d6bcf3649c161
assusdan <assusdan@yandex.ru>
2016-08-04 19:37:40 +0500

    update script, remove unneeded files

--- build.sh
+++ build.sh
@@ -10,26 +10,35 @@ then
 fi
 
 echo "Git pull..."
+echo ""
 git pull >/dev/null
+echo ""
 
 echo "Export toolchains..."
 export ARCH=arm CROSS_COMPILE=../*5.2*/bin/arm-cortex-linux-gnueabi-
+echo ""
 
 echo "Make defconfig..."
 make benefit_m7_defconfig >/dev/null
+echo ""
 
 echo "Start build..."
-time make $1 >/dev/null 2>errors.log
+time make -j4 >/dev/null 2>errors.log
+echo ""
+echo ""
 
 if [ ! -f arch/arm/boot/zImage-dtb ]
 then
-    echo "BUID ERRORS!"
-    echo "BUID ERRORS!"
-    echo "BUID ERRORS!"
+    echo "BUILD ERRORS!"
+    echo "BUILD ERRORS!"
+    echo "BUILD ERRORS!"
 else
     echo 'Moving...'
-    mv arch/arm/boot/zImage-dtb /var/www/html/boot.img-kernel
+    cp arch/arm/boot/zImage-dtb /var/www/html/boot.img-kernel
+    mv arch/arm/boot/zImage-dtb boot.img-kernel
 fi
+echo ""
+echo ""
 
 echo "======================"
 echo $[$SECONDS / 60]' minutes '$[$SECONDS % 60]' seconds' 
